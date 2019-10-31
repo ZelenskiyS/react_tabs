845806ebdf7f37c8826f218cb4be951e7db0a35b
Simon <semen.zelenskiy@gmail.com>
2019-10-31 09:18:03 +0200

    Updates

---
+++ precache-manifest.901b9e996e58b28e9816bbcac6b73b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca3ca0d61a7d044026a8d53795720b7",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "10648f6c3f104392ab58",
+    "url": "/react_tabs/static/css/main.f4be10bc.chunk.css"
+  },
+  {
+    "revision": "0bb6ab8dcd1b2c345976",
+    "url": "/react_tabs/static/js/2.fe44e0e4.chunk.js"
+  },
+  {
+    "revision": "10648f6c3f104392ab58",
+    "url": "/react_tabs/static/js/main.caee70ef.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
